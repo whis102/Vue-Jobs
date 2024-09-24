64225849435ecc85b863338bfae68db5d425e6a2
whis102 <chicongnguyen.dev@gmail.com>
2024-09-24 17:37:26 +0700

    add router

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <script setup>
 import NavBar from "./components/NavBar.vue";
-import HeroSection from "./components/HeroSection.vue";
-import Home from "./components/Home.vue";
-import JobList from "./components/JobList.vue";
+import { RouterView } from "vue-router";
 </script>
 
 <template>
   <NavBar />
-  <HeroSection />
-  <Home />
-  <JobList :limit="6" :showButton="true" />
+  <RouterView />
 </template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup>
+import HeroSection from "@/components/HeroSection.vue";
+import Home from "@/components/Home.vue";
+import JobList from "@/components/JobList.vue";
+</script>
+
+<template>
+  <HeroSection />
+  <Home />
+  <JobList :limit="6" :showButton="true" />
+</template>
