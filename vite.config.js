83d2748c082e28ecb41093d28b6ff2e2f179dd4e
whis102 <chicongnguyen.dev@gmail.com>
2024-10-13 22:20:20 +0700

    add back button

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,20 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import path from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
     port: 3000,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
   },
   resolve: {
     alias: {
