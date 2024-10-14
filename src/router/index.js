b3c2d96e183b82ef170d539d6c552857442ee21d
whis102 <chicongnguyen.dev@gmail.com>
2024-10-14 15:25:08 +0700

    complete project

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,8 @@ import HomeView from "@/views/HomeView.vue";
 import NotFoundView from "@/views/NotFoundView.vue";
 import JobListView from "@/views/JobListView.vue";
 import JobView from "@/views/JobView.vue";
+import AddJobView from "@/views/AddJobView.vue";
+import EditJobView from "@/views/EditJobView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,8 +23,20 @@ const router = createRouter({
 
     {
       path: "/jobs/:id",
-      name: "add-jobs",
+      name: "job",
       component: JobView,
+    },
+
+    {
+      path: "/jobs/edit/:id",
+      name: "edit-job",
+      component: EditJobView,
+    },
+
+    {
+      path: "/jobs/add",
+      name: "add-job",
+      component: AddJobView,
     },
     {
       path: "/:catchAll(.*)",
--- src/views/JobView.vue
+++ src/views/JobView.vue
@@ -1,16 +1,34 @@
 <script setup>
 import PulseLoader from "vue-spinner/src/PulseLoader.vue";
 import { reactive, onMounted } from "vue";
-import { useRoute, RouterLink } from "vue-router";
+import { useRoute, RouterLink, useRouter } from "vue-router";
 import BackBtn from "@/components/BackBtn.vue";
 import axios from "axios";
+import { useToast } from "vue-toastification";
 
 const route = useRoute();
+const router = useRouter();
+const toast = useToast();
+
 const jobId = route.params.id;
 const state = reactive({
   job: {},
   isLoading: true,
 });
+
+const deleteJob = async () => {
+  try {
+    const confirm = window.confirm("Are you sure?");
+    if (confirm) {
+      await axios.delete(`/api/jobs/${jobId}`);
+      toast.success("Job Deleted Successfully");
+      router.push("/jobs");
+    }
+  } catch (error) {
+    console.error("Error deleting job", error);
+    toast.error("Job Not Deleted");
+  }
+};
 
 onMounted(async () => {
   try {
@@ -38,9 +56,7 @@ onMounted(async () => {
             <div
               class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
             >
-              <i
-                class="fa-solid fa-location-dot text-lg text-orange-700 mr-2"
-              ></i>
+              <i class="pi pi-map-marker text-xl text-orange-700 mr-2"></i>
               <p class="text-orange-700">{{ state.job.location }}</p>
             </div>
           </div>
@@ -96,6 +112,7 @@ onMounted(async () => {
               >Edit Job
             </RouterLink>
             <button
+              @click="deleteJob"
               class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
             >
               Delete Job
